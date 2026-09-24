9dca643149b566ed13c3174cf727d5c96efd3ab8
rafinha1712 <mayer.rafaele@escola.pr.gov.br>
2026-09-24 10:32:07 -0300

    ll

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
+import {aleatorio} from ‘./aleatorio.js’;
+import {perguntas} from ‘./perguntas.js;
+
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
-
+const botaoJogarNovamente = document.querySelector(“.novamente-btn”);
 
 let atual = 0;
 let perguntaAtual;
@@ -36,12 +39,18 @@ function respostaSelecionada(opcaoSelecionada) {
     mostraPergunta();
 }
 
-function mostraResultado(){
-    caixaPerguntas.textContent = "Olha só o que podemos afirmar sobre você...";
-    textoResultado.textContent = historiaFinal;
-    caixaAlternativas.textContent = "";
+function mostraResultado() {
+caixaPerguntas.textContent = "Em 2049...";
+textoResultado.textContent = historiaFinal;
+caixaAlternativas.textContent = "";
+caixaResultado.classList.add("mostrar"); botaoJogarNovamente.addEventListener("click", jogaNovamente);
 }
 
-
+function jogaNovamente() {
+atual = 0;
+historiaFinal = "";
+caixaResultado.classList.remove("mostrar");
+mostraPergunta();
+}
 
 mostraPergunta();
